0599b5701b0769039eefabb39b38db52c3cb1251
Royal <thaparoyal17@gmail.com>
2021-04-30 07:58:56 +0545

    social section

---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function () {
+  let socials = document.querySelectorAll(".social div");
+
+  socials.forEach(function (social, index) {
+    social.style.animation = `moveIn 1s cubic-bezier(.51,.92,.24,1.15) forwards ${
+      index / 5 + 0.2
+    }s`;
+  });
+})();
